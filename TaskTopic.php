d4b78ea999e75d4c93b8c3ee5abc95978aa11353
Джураев Жамшед <xnemo12@mail.ru>
2019-02-12 12:08:11 +0500

    Create TaskTopic.php

---
+++ TaskTopic.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Application\AdminBundle\Topic;
+
+use Gos\Bundle\WebSocketBundle\Topic\TopicInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Wamp\Topic;
+use Gos\Bundle\WebSocketBundle\Router\WampRequest;
+
+class TaskTopic implements TopicInterface
+{
+
+    /**
+     * @param  ConnectionInterface $connection
+     * @param  Topic $topic
+     * @param WampRequest $request
+     */
+    public function onSubscribe(ConnectionInterface $connection, Topic $topic, WampRequest $request)
+    {
+        $topic->broadcast(['msg' => 'connect']);
+    }
+
+    /**
+     * @param  ConnectionInterface $connection
+     * @param  Topic $topic
+     * @param WampRequest $request
+     */
+    public function onUnSubscribe(ConnectionInterface $connection, Topic $topic, WampRequest $request)
+    {
+        $topic->broadcast(['msg' => 'disconnect']);
+    }
+
+    /**
+     * @param  ConnectionInterface $connection
+     * @param  Topic $topic
+     * @param WampRequest $request
+     * @param $event
+     * @param  array $exclude
+     * @param  array $eligible
+     */
+    public function onPublish(ConnectionInterface $connection, Topic $topic, WampRequest $request, $event, array $exclude, array $eligible)
+    {
+        $topic->broadcast([
+            'msg' => $event,
+        ]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'task.topic';
+    }
+}
